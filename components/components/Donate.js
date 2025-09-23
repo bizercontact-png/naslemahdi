7438e27ede236048f8f693b83fa91bc75b3d6482
bizercontact-png <bizer.contact@gmail.com>
2025-09-23 18:42:40 +0330

    Create Donate.js

---
+++ components/components/Donate.js
@@ -0,0 +1,8 @@
+export default function HeroSection() {
+  return (
+    <section style={{textAlign: 'center', padding: '50px'}}>
+      <h1>نسل مهدی</h1>
+      <p>کمک به مردم مظلوم، آموزش، هدایت و مسابقات علمی</p>
+    </section>
+  )
+}
