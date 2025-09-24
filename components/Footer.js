3ff4615d6cc458ace350ee753841ff2cc2424215
bizercontact-png <bizer.contact@gmail.com>
2025-09-24 14:51:01 +0330

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ padding: '20px', background: '#0f172a', color: 'white', textAlign: 'center' }}>
+      © 2025 نسل مهدی | همه حقوق محفوظ است
+    </footer>
+  )
+}
