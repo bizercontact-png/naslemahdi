a956479e4750bb06f24c1f536bf1119cc5e1b0c0
bizercontact-png <bizer.contact@gmail.com>
2025-09-24 14:30:10 +0330

    Delete components directory

--- components/components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function HeroSection() {
-  return (
-    <section style={{textAlign: 'center', padding: '50px'}}>
-      <h1>نسل مهدی</h1>
-      <p>کمک به مردم مظلوم، آموزش، هدایت و مسابقات علمی</p>
-    </section>
-  )
-}
